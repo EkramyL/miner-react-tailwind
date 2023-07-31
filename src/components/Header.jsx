da5dc9929d1bf8447d304fea67715ad8f37fa3aa
Ekramy Latief <ekramyl3000@gmail.com>
2023-07-31 21:51:29 +0300

    adding icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../images/logo.svg';
+import userIcon from '../images/user_icon.svg';
+import registerIcon from '../images/gift_icon.svg';
 
 const Header = () => {
   return (
     <div>
-      <nav className=" bg-red-700 flex  justify-between items-center text-slate-50 p-5  font-medium">
+      <nav className=" bg-[#0f172a] flex  justify-between items-center text-slate-50  py-5 px-2  font-medium">
         <Link to="/">
           <img src={logo} alt="logo" className=" h-[45px]" />
         </Link>
@@ -31,7 +33,20 @@ const Header = () => {
             </li>
           </ul>
         </div>
-        <div> User</div>
+        <div className="flex gap-3">
+          <button className="flex gap-2">
+            <i className="fill-current text-slate-50">
+              <img src={userIcon} alt="user" className="" />
+            </i>
+            <span>Login</span>
+          </button>
+          <button className="flex gap-2">
+            <i className="fill-current text-slate-50 ">
+              <img src={registerIcon} alt="user" className="h-6" />
+            </i>
+            <span>Registeration</span>
+          </button>
+        </div>
       </nav>
     </div>
   );
